611e0d0c69106a30b2bfd20de46ccd86e0bdface
yashpatel00 <yashcpatel4445@gmail.com>
2025-07-02 19:04:41 +0530

    done changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -11,7 +12,7 @@ const Navbar = () => {
       </div>
       <button
         className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-        Login
+        <Link to="/login">Login</Link>
       </button>
       
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -77,7 +77,7 @@ export function Home() {
           </button>
           <button
             className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            Register
+            <Link to="/register">Register</Link>
           </button>
         </motion.div>
         
